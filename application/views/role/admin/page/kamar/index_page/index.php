750acba5af0f7b3e37bb7c4bf1d01ce4052eaebb
Your Name <you@example.com>
2025-06-19 16:14:42 +0700

    Add link to add participants in Kamar Santri section

--- application/views/role/admin/page/kamar/index_page/index.php
+++ application/views/role/admin/page/kamar/index_page/index.php
@@ -10,6 +10,7 @@
 			<div class="panel-heading">
 				<div class="panel-title">
 					<a href="<?php echo $data_get['param']['table'] ?>/add_page" style="color:white;" class="btn btn-primary app-item"><i class="icon-plus3"></i> Tambah <?php echo $data_get['param']['title'] ?></a>
+					<a href="<?php echo $data_get['param']['table'] ?>/kamar_santri" style="color:white;" class="btn btn-success app-item"><i class="icon-users"></i> Tambah Peserta Kamar Santri</a>
 					<button class="btn btn-bulk-delete btn-danger" onclick="bulk_delete()" style="display: none;"><i class="icon-close2"></i> Hapus Data</button>
 				</div>
 				<div class="heading-elements">
